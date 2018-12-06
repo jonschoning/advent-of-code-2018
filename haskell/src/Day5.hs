47bc280870cecdb8d11895299c5715077b83da7a
Jon Schoning <jonschoning@gmail.com>
2018-12-05 22:30:42 -0600

    impl day5

--- haskell/src/Day5.hs
+++ haskell/src/Day5.hs
@@ -1,19 +1,11 @@
-{-# LANGUAGE StrictData #-}
-{-# OPTIONS_GHC -fno-warn-unused-do-bind #-}
 
 module Day5 where
 
 import qualified Data.ByteString.Char8 as B8
-import qualified Data.IntMap as M
 
-import Control.Monad.State
-import Data.Attoparsec.ByteString.Char8 as P
 import Data.ByteString (ByteString)
-import Data.Either
-import Data.IntMap (IntMap)
-import Data.List
-import Data.Maybe
-import Data.Ord
+import Data.Char (toLower)
+import Data.Foldable
 
 import Prelude
 
@@ -22,14 +14,34 @@ import Prelude
 -- | p1
 -- How many units remain after fully reacting the polymer you scanned?
 p1 :: ByteString -> Int
-p1 input =
-  let x=0
-  in 0
+p1 (readInput -> polymer) =
+  length (react polymer)
+
+react :: String -> String
+react = reverse . foldl' go []
+  where
+    go [] c = [c]
+    go xxs@(x:xs) c =
+      if x /= c && toLower x == toLower c
+        then xs
+        else c : xxs
 
 -- * Part Two
 
 -- | p2
+-- What is the length of the shortest polymer you can produce by
+-- removing all units of exactly one type and fully reacting the result?
 p2 :: ByteString -> Int
-p2 input =
-  let x=0
-  in 0
+p2 (readInput -> polymer) =
+  minimum $
+  fmap length
+    [ react polymer'
+    | x <- ['a' .. 'z']
+    , let polymer' = filter ((x /= ) . toLower) polymer
+    ]
+
+
+-- * Util
+
+readInput :: ByteString -> String
+readInput = B8.unpack . head . B8.lines
--- haskell/tests/Day5Spec.hs
+++ haskell/tests/Day5Spec.hs
@@ -14,11 +14,15 @@ spec :: Spec
 spec = do
 
     describe "Part1" $ do
+      it "sample" $ do
+        let input = "dabAcCaCBAcCcaDA"
+        D5.react (D5.readInput input) `shouldBe` "dabCBAcaDA"
+        D5.p1 input `shouldBe` 10
       it "input: file[input/day5.txt]" $ do
         input <- B8.readFile "input/day5.txt"
-        D5.p1 input `shouldBe` 0
+        D5.p1 input `shouldBe` 11264
 
-    -- describe "Part2" $ do
-    --   it "input: file[input/day5.txt]" $ do
-    --     input <- B8.readFile "input/day5.txt"
-    --     D5.p2 input `shouldBe` 128617
+    describe "Part2" $ do
+      it "input: file[input/day5.txt]" $ do
+        input <- B8.readFile "input/day5.txt"
+        D5.p2 input `shouldBe` 4552
