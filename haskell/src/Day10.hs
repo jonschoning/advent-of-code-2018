5ccc373aac1951a3f06d46d449c600410c9996a2
Jon Schoning <jonschoning@gmail.com>
2018-12-10 00:22:50 -0600

    draw sample

--- haskell/src/Day10.hs
+++ haskell/src/Day10.hs
@@ -5,7 +5,6 @@
 module Day10 where
 
 import qualified Data.ByteString.Char8 as B8
--- import qualified Data.IntMap.Strict as M
 import qualified Data.Map.Strict as M
 import Data.Attoparsec.ByteString.Char8 as P
 import qualified Data.Vector as V
@@ -20,17 +19,43 @@ import Data.Maybe
 import Debug.Trace
 import Data.Ord (comparing)
 import Control.Arrow ((&&&))
-
+import System.IO.Unsafe (unsafePerformIO)
 import Prelude
 
 -- * Part One
 
 -- | p1
 -- What is the winning Elf's score?
-p1 :: ByteString -> Int
-p1 _ =
-  let x = 0
-  in x
+p1 :: ByteString -> String
+p1 (parsePoints -> points) =
+  let ext = extents points
+      !_ = foldl' (go ext) points [0 .. 4]
+  in show (length points)
+  where
+    go ext points _ =
+      let !str = drawAscii ext points
+          !_ = unsafePerformIO (putStr $ str <> "\n")
+      in step points
+
+step :: [Point] -> [Point]
+step = fmap (\((pX, pY), v@(vX, vY)) -> ((pX + vX, pY + vY), v))
+
+extents :: [Point] -> Extent
+extents points =
+  let x = (minimum &&& maximum) (fmap (fst . fst) points)
+      y = (minimum &&& maximum) (fmap (snd . fst) points)
+  in (x, y)
+
+drawAscii :: Extent -> [Point] -> String
+drawAscii ((minX, maxX), (minY, maxY)) pointsList =
+  let pointsMap = M.fromList pointsList
+  in concat
+       [ (if x == minX then "\n" else "") <> (maybe "." (const "#") p)
+       | y <- [minY .. maxY]
+       , x <- [minX .. maxX]
+       , let p = M.lookup (x, y) pointsMap
+       ]
+
 
 -- * Part Two
 
@@ -41,3 +66,26 @@ p2 _ =
   in x
 
 -- * Util
+type Grid = M.Map P P
+type P = (Int, Int)
+type Extent = (P, P) -- * ( (minX, maxX) , (minY, maxY) )
+type Point =  (P, P)
+
+parsePoints :: ByteString -> [Point]
+parsePoints =
+  rights . fmap (parseOnly parsePoint) . B8.lines
+  where
+    parsePoint = do
+      string "position=<"
+      skipSpace
+      pX <- signed decimal
+      char ','
+      skipSpace
+      pY <- signed decimal
+      string "> velocity=<"
+      skipSpace
+      vX <- signed decimal
+      char ','
+      skipSpace
+      vY <- signed decimal
+      pure ((pX, pY), (vX, vY))
--- haskell/tests/Day10Spec.hs
+++ haskell/tests/Day10Spec.hs
@@ -15,7 +15,7 @@ spec = do
 
     describe "Part1" $ do
       it "sample" $ do
-        D10.p1 sampleInput `shouldBe` 0
+        D10.p1 sampleInput `shouldBe` ""
       -- it "input: file[input/day10.txt]" $ do
       --   input <- B8.readFile "input/day10.txt"
       --   D10.p1 input `shouldBe` 0
@@ -26,4 +26,35 @@ spec = do
     --     D10.p2 input `shouldBe` 0
 
 sampleInput :: B8.ByteString
-sampleInput = "" 
+sampleInput = B8.unlines
+  [ "position=< 7,  0> velocity=<-1,  0>" 
+  , "position=< 3, -2> velocity=<-1,  1>"
+  , "position=< 6, 10> velocity=<-2, -1>"
+  , "position=< 2, -4> velocity=< 2,  2>"
+  , "position=<-6, 10> velocity=< 2, -2>"
+  , "position=< 1,  8> velocity=< 1, -1>"
+  , "position=< 1,  7> velocity=< 1,  0>"
+  , "position=<-3, 11> velocity=< 1, -2>"
+  , "position=< 7,  6> velocity=<-1, -1>"
+  , "position=<-2,  3> velocity=< 1,  0>"
+  , "position=<-4,  3> velocity=< 2,  0>"
+  , "position=<10, -3> velocity=<-1,  1>"
+  , "position=< 5, 11> velocity=< 1, -2>"
+  , "position=< 4,  7> velocity=< 0, -1>"
+  , "position=< 8, -2> velocity=< 0,  1>"
+  , "position=<15,  0> velocity=<-2,  0>"
+  , "position=< 1,  6> velocity=< 1,  0>"
+  , "position=< 8,  9> velocity=< 0, -1>"
+  , "position=< 3,  3> velocity=<-1,  1>"
+  , "position=< 0,  5> velocity=< 0, -1>"
+  , "position=<-2,  2> velocity=< 2,  0>"
+  , "position=< 5, -2> velocity=< 1,  2>"
+  , "position=< 1,  4> velocity=< 2,  1>"
+  , "position=<-2,  7> velocity=< 2, -2>"
+  , "position=< 3,  6> velocity=<-1, -1>"
+  , "position=< 5,  0> velocity=< 1,  0>"
+  , "position=<-6,  0> velocity=< 2,  0>"
+  , "position=< 5,  9> velocity=< 1, -2>"
+  , "position=<14,  7> velocity=<-2,  0>"
+  , "position=<-3,  6> velocity=< 2, -1>" 
+  ]
